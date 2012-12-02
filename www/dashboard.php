2768967cc3b5c963e7baa7dff18572f47e444e34
Nuno Rodrigues <nunomorenorodrigues@gmail.com>
2012-12-02 09:25:03 +0000

    Choosepic as Main

--- www/dashboard.php
+++ www/dashboard.php
@@ -6,13 +6,8 @@ if (isset($_SESSION['redirect']) && $_SESSION['redirect'] != '') {
     header("Location: " . $_SESSION['redirect']);
     exit();
 }
-$li = initLinkedin();
 
-$result = retrieveRandomConnection($_SESSION['linkedinId']); 
-
-$s->assign('users', $result);
-$s->display('header.tpl');
-$s->display('dashboard.tpl');
-$s->display('footer.tpl');
-
+header("Location: choosepic.php");
+exit();
+    
 ?>
